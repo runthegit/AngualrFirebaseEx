bee5a33cabe66968a29fb97f1ad1f06166d54d34
runthegit <36576757+runthegit@users.noreply.github.com>
2019-05-03 10:43:53 -0500

    suppose to attach firebase auth and angular mats

--- AngularFirebaseEx/src/environments/environment.ts
+++ AngularFirebaseEx/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDoarEAY2ZQKrVB-KHK1Y0itHnv6fKnc1g",
+    authDomain: "angularfirebaseproj-ec38d.firebaseapp.com",
+    databaseURL: "https://angularfirebaseproj-ec38d.firebaseio.com",
+    projectId: "angularfirebaseproj-ec38d",
+    storageBucket: "angularfirebaseproj-ec38d.appspot.com",
+    messagingSenderId: "407223919330"
+  }
 };
 
 /*
